d39730c56872682d38604fa9fe84be677aff45a1
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2020-06-14 10:23:21 -0400

    Initial Commit

--- NinjaCheetah Installer/Form1.Designer.cs
+++ NinjaCheetah Installer/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
