2acdec87d95cd8f262bb3c03ad003b0d7b80c51e
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2020-07-08 19:15:32 -0400

    Revision to v2.3: fixed a form not terminating the program correctly

--- NinjaCheetah Installer/Form13.Designer.cs
+++ NinjaCheetah Installer/Form13.Designer.cs
@@ -38,7 +38,7 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 58);
+            this.button1.Size = new System.Drawing.Size(189, 67);
             this.button1.TabIndex = 0;
             this.button1.Text = "Visual Basic";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,9 +47,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(12, 80);
+            this.button2.Location = new System.Drawing.Point(12, 85);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(173, 58);
+            this.button2.Size = new System.Drawing.Size(189, 75);
             this.button2.TabIndex = 1;
             this.button2.Text = "Batch";
             this.button2.UseVisualStyleBackColor = true;
@@ -59,7 +59,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(197, 150);
+            this.ClientSize = new System.Drawing.Size(213, 172);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
