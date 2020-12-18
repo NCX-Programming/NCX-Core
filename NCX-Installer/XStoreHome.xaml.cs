dfd83a755c87de8d0065ac3b8f4dc82ec128a1d6
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2020-12-18 07:48:34 -0500

    Tried some stuff, didn't work, will try more later

--- NCX-Installer/XStoreHome.xaml.cs
+++ NCX-Installer/XStoreHome.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO.Compression;
+using Newtonsoft.Json;
+using System.Windows.Resources;
 
 namespace NCX_Installer
 {
@@ -22,6 +24,20 @@ namespace NCX_Installer
     /// </summary>
     public partial class XStoreHome : Page
     {
+        static readonly string SavePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public string icon;
+        public int slot;
+
+        public class Store
+        {
+            public string name1 { get; set; }
+            public string auth1 { get; set; }
+            public string desc1 { get; set; }
+            public string icon1 { get; set; }
+            public string down1 { get; set; }
+            public string file1 { get; set; }
+        }
+
         public XStoreHome()
         {
             InitializeComponent();
@@ -29,6 +45,14 @@ namespace NCX_Installer
             {
                 this.Background = Brushes.White;
                 label1.Foreground = Brushes.Black; label2.Foreground = Brushes.Black; btn6.Foreground = Brushes.Black;
+            }
+            if (File.Exists(System.IO.Path.Combine(SavePath, "XStore.json")))
+            {
+                string json = File.ReadAllText(System.IO.Path.Combine(SavePath, "XStore.json"));
+                Store store = JsonConvert.DeserializeObject<Store>(json);
+
+                slot = 1;
+                DownloadIcon(store.icon1);
             }
         }
 
@@ -78,6 +102,41 @@ namespace NCX_Installer
         {
             About page = new About();
             NavigationService.Navigate(page);
+        }
+
+        public void DownloadIcon(string link)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadFileCompleted += DownloadCompleted;
+                wc.DownloadFileAsync(
+                    // Param1 = Link of file
+                    new System.Uri($"{link}"),
+                    // Param2 = Path to save
+                    System.IO.Path.Combine(SavePath, $"NCX-Core/slot{slot}.png")
+                );
+            }
+        }
+
+        public void DownloadCompleted(object sender, EventArgs e)
+        {
+            // icon = System.IO.Path.Combine(SavePath, $"NCX-Core/slot1.png");
+            Uri resourceUri = new Uri(System.IO.Path.Combine(SavePath, $"NCX-Core/slot1.png"), UriKind.Absolute);
+            StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
+            BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+            var brush = new ImageBrush();
+            brush.ImageSource = temp;
+            switch (slot) {
+                case 1:
+                    tmpbtn1.Background = brush;
+                    break;
+                case 2:
+                    tmpbtn2.Background = brush;
+                    break;
+                case 3: 
+                    tmpbtn3.Background = brush;
+                    break;
+            } 
         }
     }
 }
